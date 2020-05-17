3d25efc31aae2f7e6764de8182dbae56c50401fb
sarahstrickman <sarahstrickman@gmail.com>
2020-05-17 19:31:49 -0400

    tasks for week 1

---
+++ Modules/Module1/w1d1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace w1d1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Modules/Module1/w1d2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace w1d2
+{
+    public class DirThing
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            var start = DateTime.Now;
+            var di = Directory.GetFiles(".");
+            Console.WriteLine("Program arguments:\t" + args);
+            if(args.Length == 1)
+            {
+                di = Directory.GetFiles(args[0]);
+            }
+            
+
+            foreach (var d in di)
+            {
+                var f = new FileInfo(d);
+                var fname = "";
+                f.Refresh();
+                
+                fname = Path.GetFileName(f.FullName);    // get just the name of the file
+                
+                var fileTime = f.LastWriteTime.ToString("MMM dd HH:mm");
+                var ft = f.LastWriteTime;
+                var interval = DateTime.Now - ft;
+                if (interval.Days > 365)
+                {
+                    fileTime = f.LastWriteTime.ToString("yyyy");
+                }
+                Console.WriteLine("{0, 10} {1} {2}", f.Length, fileTime, fname);
+            }
+
+            var end = DateTime.Now;
+            Console.WriteLine(end-start);
+            Console.ReadKey();
+
+        }
+    }    
+}
