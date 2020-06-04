2189106883f7d42f37f52fd279da49cb4190549a
Sarah Strickman <sarahstrickman@gmail.com>
2020-06-04 11:25:06 -0400

    sruff copied to other folder

--- Projects/Project1/du/Program.cs
+++ Projects/Project1/du/Program.cs
@@ -1,45 +1,36 @@
-﻿using System;
+﻿/*
+ * Contains main.
+ *
+ * author :    Sarah Strickman
+ *             sxs4599@rit.edu
+ */
+
+using System;
 using System.Threading;
 using System.Collections.Generic;
 
 namespace du
 {
-
-    class SequentialOperation {
-        public int[] calculateSeq(String dir) {
-            var numFiles = 0;   // the number of files calculated
-            var numDirs = 0;    // the number of directories calculated
-            var byteSize = 0;   // the total size (in bytes)
-
-            // if it's a file
-                // numFiles++
-                // get size of file
-            var numberList = new int[3];
-            return numberList;
-        }
-    }
-
     class Program
     {
-
-        
-
         static void Main(string[] args)
         {
             // check args
-            if (args.Length != 2) {
+            if (args.Length != 2)
+            {
                 Console.WriteLine("Usage: du [-s] [-p] [-b]\n"+
                 "You MUST specify one of the parameters -s, -p, or -b.\n"+
                 "-s\tRun in sequential mode\n"+
-                "-p\tRun in parallel bode (uses all available processors)"+
+                "-p\tRun in parallel bode (uses all available processors)\n"+
                 "-b\tRuns in parallel, followed by sequential mode");
                 Environment.Exit(0);
             }
 
-            if (args[0] == "-p") {
+            if (args[0] == "-p")
+            {
                 var parTime = 0.0;
                 var watch = System.Diagnostics.Stopwatch.StartNew();
-
+        
                 // run project in parallel
                 
                 watch.Stop();
@@ -47,14 +38,20 @@ namespace du
                 Console.WriteLine("Parallel calculated in: " + parTime + "s");
             }
             if (args[0] == "-s") {
+                Console.WriteLine("Calculating usage data for " + args[1] + "\n");
                 var seqTime = 0.0;
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 
-                // run project sequentially
-                
+                SequentialOperation s = new SequentialOperation();
+                s.Calculate(args[1]);
+
                 watch.Stop();
+                seqTime = watch.Elapsed.TotalMilliseconds / 1000.0;
                 seqTime = Math.Round(seqTime, 7);
                 Console.WriteLine("Sequential calculated in: " + seqTime + "s");
+                Console.WriteLine("Number of files:\t" + s.NumFiles);
+                Console.WriteLine("Number of directories:\t" + s.NumDirs);
+                Console.WriteLine("Total size in bytes:\t" + s.NumBytes);
             }
             if (args[0] == "-b") {
                 var seqTime = 0.0;  // length of time for sequential execution
